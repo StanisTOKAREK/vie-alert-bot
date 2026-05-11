5f36d4fb260340f2244728338e1733d721099722
Stanis.Tokarek <stanis.tokarek1@decathlon.com>
2026-05-11 21:52:50 +0200

    Setup: Fix launchd config with Python 3.11 from Homebrew - now working

--- run_check.py
+++ run_check.py
@@ -3,6 +3,4 @@ from vie_alert_bot import main
 
 
 if __name__ == "__main__":
-    # Pass all CLI arguments to main()
-    hourly = "--hourly" in sys.argv
-    raise SystemExit(main(hourly=hourly))
+    raise SystemExit(main())
